3ad582f88f342ed6b04718f251bd9fb5f3db86d9
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-27 13:11:26 +0430

    Add route model binding in testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ class TestCase extends BaseTestCase
             ['--database' => 'testbench']
         )->run();
 
-        LaravelTickets::api();
+        LaravelTickets::api(['middleware' => 'bindings']);
         LaravelTickets::routes();
     }
 
