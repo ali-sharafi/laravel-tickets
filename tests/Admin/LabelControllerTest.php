eb50b7c9b8d477ca09ec8aea2056550f5b67e854
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-27 13:31:02 +0430

    Add ticket label test

---
+++ tests/Admin/LabelControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace LaravelTickets\Tests;
+
+use LaravelTickets\Models\TicketLabel;
+
+class LabelControllerTest extends TestCase
+{
+    /** @test */
+    public function test_can_add_new_label()
+    {
+        $label = factory(TicketLabel::class)->make()->toArray();
+
+        $this->post('/tickets/labels', $label)
+            ->assertStatus(200)
+            ->assertJson(['status' => 'success', 'data' => [$label]]);
+
+        $this->assertDatabaseHas('ticket_labels', $label);
+    }
+
+    /** @test */
+    public function test_can_get_labels()
+    {
+        $label = factory(TicketLabel::class)->create()->toArray();
+
+        $this->get('/tickets/labels')
+            ->assertStatus(200)
+            ->assertJson(['status' => 'success', 'data' => [$label]]);
+    }
+
+    /** @test */
+    public function test_can_update_a_label()
+    {
+        $label = factory(TicketLabel::class)->create();
+        $updatedLabel = factory(TicketLabel::class)->make()->toArray();
+
+        $this->patch('/tickets/labels/' . $label->id, $updatedLabel)
+            ->assertStatus(200)
+            ->assertJson(['status' => 'success', 'data' => [$updatedLabel]]);
+    }
+
+    /** @test */
+    public function test_can_delete_a_label()
+    {
+        $label = factory(TicketLabel::class)->create();
+
+        $this->delete('/tickets/labels/' . $label->id)
+            ->assertStatus(200)
+            ->assertJson(['status' => 'success']);
+
+        $this->assertDatabaseCount('ticket_labels', 0);
+    }
+}
