4fbddf514f69201550b00578325cdae1071bb920
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-04 22:11:29 +0430

    Add web middleware to default middlewares

--- src/LaravelTickets.php
+++ src/LaravelTickets.php
@@ -14,6 +14,7 @@ class LaravelTickets
 
         $defaultOptions = [
             'prefix' => 'tickets',
+            'middleware' => 'web',
             'namespace' => '\LaravelTickets\Http\Controllers',
         ];
 
